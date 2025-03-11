2f667594cf8cf2925abf74e92c2d4f522b348982
Manuel Graziani <manuelgraziani42@gmail.com>
2025-03-11 19:37:05 +0100

    feat: add functionality of add a task

--- index.js
+++ index.js
@@ -14,4 +14,31 @@ if(!fs.existsSync(taskFile)){
     fs.writeFileSync(taskFile, JSON.stringify([]))
 }
 
+function readTask() {
+    if(fs.existsSync(taskFile)) {
+        const data = fs.readFileSync(taskFile, "utf-8")
+        return JSON.parse(data)
+    } 
+    return []
+}
 
+function addTask(description) {
+    const tasks = readTask()
+    const newTask = {
+        id: 1,
+        description: description,
+        status: "todo",
+    }
+    tasks.push(newTask)
+    fs.writeFileSync(taskFile, JSON.stringify(tasks), "utf-8")
+}
+
+if(args[0] === "add"){
+    const taskDescription = args.slice(1).join("")
+    if(!taskDescription){
+        console.log("Please provide a description");
+        console.log("Sample: task-cli add <description>");
+    } else {
+        addTask(taskDescription)
+    }
+} 
