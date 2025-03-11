12b09d7979d3668eb57e48af564d3e6060279707
Manuel Graziani <manuelgraziani42@gmail.com>
2025-03-11 19:18:23 +0100

    chore: add if condition to check if taskFile exists when running the app

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 #!/usr/bin/env node
+import fs from 'fs'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+const taskFile = path.join(__dirname, "tasks.json")
+
+const args = process.argv.slice(2)
+
+if(!fs.existsSync(taskFile)){
+    fs.writeFileSync(taskFile, JSON.stringify([]))
+}
+
 
