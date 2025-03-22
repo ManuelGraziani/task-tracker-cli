09041642128f97cc90df40fd8cf687bb9f4b46aa
Manuel Graziani <manuelgraziani42@gmail.com>
2025-03-22 18:12:49 +0100

    chore: add yargs and refactor commands

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 #!/usr/bin/env node
 import fs from "fs";
+import yargs from "yargs";
+import { hideBin } from "yargs/helpers";
 import { addTask } from "./utils/addTask.js";
 import { listTasks } from "./utils/listTask.js";
 import { updateTask } from "./utils/updateTask.js";
 import { deleteTask } from "./utils/deleteTask.js";
 import { markTask } from "./utils/markTask.js";
 import { taskFile } from "./utils/readTask.js";
+import { argv } from "process";
 
 const args = process.argv.slice(2);
 
@@ -13,57 +16,89 @@ if (!fs.existsSync(taskFile)) {
   fs.writeFileSync(taskFile, JSON.stringify([]));
 }
 
-if (args[0] === "add") {
-  const taskDescription = args.slice(1).join("");
-  if (!taskDescription) {
-    console.log(
-      "Please provide a description\nSample: task-cli add <description>"
-    );
-  } else {
-    addTask(taskDescription);
-  }
-} else if (args[0] === "list") {
-  const status = args[1];
-  listTasks(status);
-} else if (args[0] === "update") {
-  const id = args[1];
-  const newDescription = args.slice(2).join(" ");
-  if (!id || !newDescription) {
-    console.log(
-      "Please provide an id and a new description\nSample: task-cli update <id> <description>"
-    );
-  } else {
-    updateTask(id, newDescription);
-  }
-} else if (args[0] === "delete") {
-  const id = args[1];
-  if (!id) {
-    console.log("Please provide an id\nSample: task-cli delete <id>");
-  } else {
-    deleteTask(id);
-  }
-} else if (args[0] === "mark-in-progress") {
-  const id = args[1];
-  if (!id) {
-    console.log("Please provide an id\nSample: task-cli mark-in-progress <id>");
-  } else {
-    markTask(id, "in-progress");
-  }
-} else if (args[0] === "mark-done") {
-  const id = args[1];
-  if (!id) {
-    console.log("Please provide an id\nSample: task-cli mark-in-progress <id>");
-  } else {
-    markTask(id, "done");
-  }
-} else {
-  console.log(
-    "Please provide a command. Available commands:\n" +
-    "  add <description>          - Add a new task\n" +
-    "  list [status]              - List tasks, optionally filtered by status (todo, done, in-progress)\n" +
-    "  update <id> <description>  - Update the description of a task\n" +
-    "  delete <id>                - Delete a task by ID\n" +
-    "  mark-in-progress <id>      - Mark a task as in-progress\n" +
-    "  mark-done <id>             - Mark a task as done"
-  );
-}
+yargs(hideBin(process.argv))
+  .command(
+    "add <description>",
+    "Add a new task",
+    (yargs) => {
+      yargs.positional("description", {
+        describe: "Description of the task",
+        type: "string",
+      });
+    },
+    (argv) => {
+      addTask(argv.description);
+    }
+  )
+  .command(
+    "list [status]",
+    "List tasks, optionally filtered by status",
+    (yargs) => {
+      yargs.positional("status", {
+        describe: "Status of the tasks to list (todo, done, in-progress)",
+        type: "string",
+      });
+    },
+    (argv) => {
+      listTasks(argv.status);
+    }
+  )
+  .command(
+    "update <id> <description>",
+    "Update the description of a task",
+    (yargs) => {
+      yargs
+        .positional("id", {
+          describe: "ID of the task to update",
+          type: "string",
+        })
+        .positional("description", {
+          describe: "New description of the task",
+          type: "string",
+        });
+    },
+    (argv) => {
+      updateTask(argv.id, argv.description);
+    }
+  )
+  .command(
+    "delete <id>",
+    "Delete a task",
+    (yargs) => {
+      yargs.positional("id", {
+        describe: "ID of the task to delete",
+        type: "string",
+      });
+    },
+    (argv) => {
+      deleteTask(argv.id);
+    }
+  )
+  .command(
+    "mark-in-progress <id>",
+    "Mark a task as in-progress",
+    (yargs) => {
+      yargs.positional("id", {
+        describe: "ID of the task to mark as in-progress",
+        type: "string",
+      });
+    },
+    (argv) => {
+      markTask(argv.id, "in-progress");
+    }
+  )
+  .command(
+    "mark-done <id>",
+    "Mark a task as done",
+    (yargs) => {
+      yargs.positional("id", {
+        describe: "ID of the task to mark as done",
+        type: "string",
+      });
+    },
+    (argv) => {
+      markTask(argv.id, "done");
+    }
+  )
+  .demandCommand(1, "Please provide a valid command.")
+  .help().argv;
